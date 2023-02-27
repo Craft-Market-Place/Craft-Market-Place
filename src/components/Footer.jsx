18c91d012e4b5fb30494c435a6d13f47c1bbf989
Rocio-Leiva <rmleipec@gmail.com>
2023-02-27 18:02:26 +0100

    added change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,7 @@ export const Footer = () => {
       <div className="footerContainer">
         <h5 className= "footerText">© Meraky 2023</h5>
         <Link to={'/warning'}><h5 className= "footerText">Aviso legal</h5></Link>
-        {/* <Link to='/legalstuff' className="footerText">Aviso Legal</Link> */}
         <Link to={'/cookies'}><h5 className= "footerText">Política de cookies</h5></Link>
-        <div className='footerSocialNetworks' >
-            <img className='socialNetworks' src="/src/img/icon-facebook.png" alt="Icono de Facebook" />
-            <img className='socialNetworks' src="/src/img/icon-linkedin.png" alt="Icono de LinkedIn" />
-            <img className='socialNetworks' src="/src/img/icon-twitter.png" alt="Icono de Twitter" />
-        </div>
       </div>
     </footer>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
-import logo from '../assets/logo.svg';
+import logo from '../assets/craft4.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,7 +10,7 @@ return (
     <Navbar bg="light" expand="xl" className='navigation'>
         <Container className='menu'>
         <Navbar.Brand to="/">
-            <NavLink to="/" className="navbar-home"><img src={logo} alt="" className='logo' /></NavLink>
+            <NavLink to="/" className="navbar-home"><img src={logo} alt="icon craft market place"  className='logo' /></NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
