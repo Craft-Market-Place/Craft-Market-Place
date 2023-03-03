351003baface5cb0c77d733ab1e8685722f39fad
Margarita Álvarez González <karessibienestar@gmail.com>
2023-03-03 11:20:23 +0100

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import "./Footer.css"
+import Nav from 'react-bootstrap/Nav';
 
 export const Footer = () => {
 
   return (
-    <footer>
-      <div className="footerContainer">
-        <h5 className= "footerText">© Craft 2023 Spain S.L. Free bulletin board</h5>
-        <h5 className= "footerText">Legal</h5>
-        <h5 className= "footerText">Cookie Settings</h5>
-        <h5 className= "footerText">Terms & Conditions</h5>
-        <h5 className= "footerText">Privacy</h5>
-        <h5 className= "footerText">Returns</h5>
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item >
+        <Nav.Link className='footer-text' href="/home">© Craft Marketplace 2023 Spain S.L. Free bulletin board</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link className='footer-text' eventKey="link-1">Privacy Policy Cookies</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link  className='footer-text' eventKey="link-2"  >Terms & Conditions </Nav.Link>
+       
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link className='footer-text' eventKey="disabled" disabled>
+          About Craft
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  //   <footer>
+  //     <div className="footerContainer">
+  //       <h5 className= "footerText">© Craft Marketplace 2023 Spain S.L. Free bulletin board</h5>
+  //       <h5 className= "footerText">Legal</h5>
+  //       <h5 className= "footerText">Cookie Settings</h5>
+  //       <h5 className= "footerText">Terms & Conditions</h5>
+  //       <h5 className= "footerText">Privacy</h5>
+  //       <h5 className= "footerText">Returns</h5>
         
-      </div>
-    </footer>
-  )
+  //     </div>
+  //   </footer>
+ )
 }
 export default Footer
