2305c4892e7f7a6044a5b9b25668d8bdea56cb46
Margarita Álvarez González <karessibienestar@gmail.com>
2023-03-04 14:25:33 +0100

    changed style

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -15,19 +15,15 @@ const Post = () => {
         <h1 className="title-ad">{post.title}</h1>
         <div className="ad-title d-flex justify-content-between">
           <h3><img src={Location} id="logo-location"/>{post.location}</h3>
-          <h5 className="price"><img src={Price} id="logo-location"/>{post.price}</h5>
+          <h3 className="price"><img src={Price} id="price1"/>{post.price}</h3>
           </div>
           <div>
           <h5 className="description">{post.description}</h5>
-          
           </div>
-          <h5 className="user">{post.user}</h5>
+          <h3 className="units">Units available: {post.units}</h3>
+          <h4 className="user"> Seller:{post.user}</h4>
         
-        <p style={{lineHeight: 1}}>{post.body}</p>
-        <div className="ads-user">
-            <img className="rounded-circle" src={post.user} alt="" />
-          <h6> {post.userName}</h6>
-        </div>
+        
       </div>
     </div>
   </section>  
